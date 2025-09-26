0840b97a44190b3283a398526a7c853241f04ae2
Oliver_says <70497019+Oliver1096@users.noreply.github.com>
2025-09-26 15:10:03 -0600

    Add files via upload

---
+++ CRR_european_option_valuation.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+"""
+Cox-Ross-Rubinstein Binomial Model
+European Option Valuation
+"""
+
+import math
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+mpl.rcParams['font.family'] = 'serif'
+from 
